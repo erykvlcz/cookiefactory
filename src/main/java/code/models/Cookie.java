fe61d7b2cef09b3f2252c0867bb47152d2fe6f31
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-06-19 14:00:05 +0200

    added Cookie

---
+++ src/main/java/code/models/Cookie.java
@@ -0,0 +1,46 @@
+package code.models;
+
+public class Cookie {
+    private String name;
+    private String taste;
+    private Integer calories;
+
+    public Cookie(String name, String taste, Integer calories) {
+        this.name = name;
+        this.taste = taste;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return "Cookie{" +
+                "name='" + name + '\'' +
+                ", taste='" + taste + '\'' +
+                ", calories=" + calories +
+                '}';
+    }
+}
