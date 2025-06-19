008f8ea89e4753f9d2b65ffb95057bcb227e3280
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-06-19 14:53:05 +0200

    added things from task5 to task 6

--- src/main/java/code/models/Client.java
+++ src/main/java/code/models/Client.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Client {
     private String name;
     private ArrayList<Cookie> ownedCookies = new ArrayList<>();
+    Cookie favCookie;
 
     public Client(String name) {
         this.name = name;
@@ -20,6 +21,14 @@ public class Client {
 
     public void addToCookieToList(Cookie cookie){
         ownedCookies.add(cookie);
+    }
+
+    public Cookie getFavCookie() {
+        return favCookie;
+    }
+
+    public void setFavCookie(Cookie favCookie) {
+        this.favCookie = favCookie;
     }
 
     @Override
