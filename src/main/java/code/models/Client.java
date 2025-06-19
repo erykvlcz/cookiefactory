cc47c59ad18b942d33c9deb90a6608bf9dd0fd2f
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-06-19 15:08:23 +0200

    edited addCookieToList() Client

--- src/main/java/code/models/Client.java
+++ src/main/java/code/models/Client.java
@@ -21,6 +21,7 @@ public class Client {
 
     public void addToCookieToList(Cookie cookie){
         ownedCookies.add(cookie);
+        System.out.println("Dodano: " + cookie);
     }
 
     public Cookie getFavCookie() {
